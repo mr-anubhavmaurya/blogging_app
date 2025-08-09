a849c7de9d70602911dd062ad72b17acda60f458
Anubhav Maurya <98anubhav.infotech@gmail.com>
2025-08-09 20:22:48 +0530

    completed user routes and jwt authentication

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const path = require("path")
+const path = require("path");
 const PORT = process.env.PORT;
-const userRoute = require("./routes/user")
-const db = require("./connection")
+const userRoute = require("./routes/user");
+const db = require("./connection");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { checkAuthenticationCookie } = require("./middlewares/authentication");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 //middleware
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(checkAuthenticationCookie("token"));
+// app.use((err,req,res,next)=>{
+//   console.log(err);
+//   res.status(500).send("something broke!")
+// })
 
-app.get("/", (req,res)=>{
-    res.render("home");
-})
+app.get("/", (req, res) => {
+  res.render("home", {
+    user: req.user,
+  });
+});
 // routes
 app.use("/user", userRoute);
 
-app.listen(PORT,()=>{
-    console.log(`server running at:- http://localhost:${PORT}`)
-} )
+app.listen(PORT, () => {
+  console.log(`server running at:- http://localhost:${PORT}`);
+});
---
+++ services/jwtAuth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const {randomBytes} = require("crypto")
+const secret = randomBytes(16).toString();
+console.log("secret:", secret);
+// const secret = "anubhav"
+
+
+function verifyToken(token){
+    const payload = jwt.verify(token,secret);
+    return payload;
+    
+}
+function generateToken(user){
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        role: user.role,
+        profileImageURL: user.profileImageURL
+    }
+    
+    const token = jwt.sign(payload,secret);
+    // console.log("token",token)
+    return token;
+}
+
+module.exports = {generateToken, verifyToken}
